8e55542fab08f5214ff2f6d2cacc5a5c3bf40340
James Finkel <32717956+jaf177@users.noreply.github.com>
2018-02-19 02:14:05 -0500

    Done

--- Seed_test.rb
+++ Seed_test.rb
@@ -6,7 +6,7 @@ class SeedTest < Minitest::Test
 #TESTS Seed.rb
 #5 should be a legal seed, seed = 5
 def test_seed_legal
-  s = Seed.new([5])
+  s = Seed.new(["5"])
   assert_equal s.arg,5
 end
 #Dog should be an illegal seed, seed = 0
@@ -14,15 +14,15 @@ def test_seed_illegal
   s = Seed.new(["dog"])
   assert_equal s.arg,0
 end
-#5.8 should be a legal seed, seed = 5
+#5.8 should be an illegal seed, seed = 0
 def test_seed_double
-  s = Seed.new([5.8])
-  assert_equal s.arg,5
+  s = Seed.new(["5.8"])
+  assert_equal s.arg,0
 end
 #Dog,5 should be an illegal seed, seed = QUIT
 #EDGE CASE
 def test_multiple_inputs
-  s = Seed.new(["Dog",5])
+  s = Seed.new(["Dog","5"])
   assert_equal "QUIT",s.arg
 end
 # should be an illegal seed, seed = QUIT
